20c6990eb350901b62a4b0935502e0eaec23edac
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-06-10 13:41:16 -0500

    Incorporates grid object to the struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,13 +8,14 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
+    grid = hex
     # TODO: Change this to non deprecaated Enum.chunk_every()
     |> Enum.chunk(3)
     |> Enum.map(&mirror_row/1)
     |> List.flatten
     |> Enum.with_index
 
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
