180fa002d5493e4b89cc7c58cf2d3f63a62f2d30
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-06-10 13:35:48 -0500

    Adds an index to the grid builder

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -12,6 +12,8 @@ defmodule Identicon do
     # TODO: Change this to non deprecaated Enum.chunk_every()
     |> Enum.chunk(3)
     |> Enum.map(&mirror_row/1)
+    |> List.flatten
+    |> Enum.with_index
 
   end
 
