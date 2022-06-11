d916425f251fb950d4de02ce62df58d5e1f42923
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-06-11 15:09:34 -0500

    Incorporates pixel_map to struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -10,7 +10,7 @@ defmodule Identicon do
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
-    Enum.map grid, fn({_code, index}) ->
+    pixel_map = Enum.map grid, fn({_code, index}) ->
       horizontal = rem(index, 5) * 50
       vertical = div(index, 5) * 50
 
@@ -19,6 +19,8 @@ defmodule Identicon do
 
       {top_left, bottom_right}
     end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
 
   end
 
